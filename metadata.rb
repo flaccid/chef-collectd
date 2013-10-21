675091f68cec114bee3bdd1f033a390234eaccdf
Chris Fordham <chris@xhost.com.au>
2013-10-21 18:27:36 +1100

    Add name attribute to metadata and remove old/duplicate supports attribute.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-maintainer       "Noan Kantrowitz"
-maintainer_email "noah@coderanger.net"
-license          "Apache 2.0"
-description      "Install and configure the collectd monitoring daemon"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
-supports         "ubuntu"
+name              "collectd"
+maintainer        "Noan Kantrowitz"
+maintainer_email  "noah@coderanger.net"
+license           "Apache 2.0"
+description       "Install and configure the collectd monitoring daemon"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
 
 depends "apache2"
 
